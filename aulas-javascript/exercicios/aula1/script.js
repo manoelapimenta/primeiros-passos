f948617ed033ebca106e51a8068eb6b8d42a0fa5
Manoela Pimenta <manoela.pimenta@gmail.com>
2020-09-24 16:59:27 -0300

    Exercício 1 - JavaScript

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,12 @@
+function checarsePodeDirigir () {
+
+    let idade = prompt ('Digite uma idade');
+    
+        if (idade >= 18){
+        alert ('Pode Dirigir!');
+        }
+        else {
+        alert ('Não Pode Dirigir!');
+        }
+}
+checarsePodeDirigir ();
